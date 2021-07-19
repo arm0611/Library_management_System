7c84d986653cb6f3ad8addb196b00131d96eaa67
arm0611 <akshayrmud@gmail.com>
2021-07-19 20:15:58 +0530

    Final code (added comments )

--- OOP_Exp_1.cpp
+++ OOP_Exp_1.cpp
@@ -1,3 +1,8 @@
+/* This program uses array of structures to implement a Library Management System. There is a switch-case
+which allows the user to chose whether to add, display, search or sort the list of books. Searching can be
+done on the basis of author name or book name. When a case is chosen, it calls the corresponding function
+to perform the task. 
+*/
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -6,104 +11,106 @@ int n;
 
 struct Library
 {
-    char book_name[100] , auth_name[100];
-    int no_copies;
-    float book_price;
+    char book_name[100] , auth_name[100];    //To hold the name of book and author respectively
+    int no_copies;                           //Number of copies
+    float book_price;                        //The book price . It is in float as price can be in decimal.
 }s[1000];
 
 
-void Add_details()
+void Add_details()                          // Function to enter the list
 {
     cout << "Enter the number of entries you wish to make " <<endl;
-    cin >> n;
+    cin >> n;                              // How many entries user wants to make
     
-    if(n >= 0 && n <= 1000 )
+    if(n >= 0 && n <= 1000 )               // To check for underflow and overflow 
     {
         for(int i=0; i<n; i++)
         {
-            cout << "Enter the name of book "<< i+1 <<endl;
+            cout << "Enter the name of book "<< i+1 <<" :" <<endl;
             cin  >> s[i].book_name;
-            cout <<"Enter the Author of book "<< i+1 <<endl;
+            cout <<"Enter the Author of book "<< i+1 <<" :" <<endl;
             cin  >> s[i].auth_name;
-            cout <<"Enter the price of book  "<< i+1 <<endl;
+            cout <<"Enter the price of book  "<< i+1 <<" :" <<endl;
             cin  >> s[i].book_price;
-            cout << "Enter the number of copies of book "<< i+1 <<endl;
+            cout << "Enter the number of copies of book "<< i+1 <<" :" <<endl;
             cin  >> s[i].no_copies;
         }
         cout<<"Successfully entered all the details !"<<endl;
     }
     else 
     {
-        cout << "Error ! Please try again. " << endl ;
+        cout << "Error ! Please try again. " << endl ;  //error message if not successful
     }
     
 }
 
 
-void Display()
+void Display()     //Function to Display the list
 {
-    cout<<"\t Book Name \t\t Author Name \t\t Price \t\t Number of copies "<<endl;
+    cout<<"\t\t\t Book Name \t\t\t Author Name \t\t\t Price \t\t\t Number of copies "<<endl;
     for (int i=0; i<n; i++)
     {
         cout << i+1 << "\t\t " << s[i].book_name<< "\t\t  " << s[i].auth_name<< "\t\t  " << s[i].book_price<< "\t\t " << s[i].no_copies << endl;
     }
+    cout<<endl;   //for aesthetic purposes
+    cout<<endl;
 }
 
 
-void Search_book ()
+void Search_book ()  //Function to search using book name
 {
-    int flag;
-    char search_bname[100];
-    cout << "Please enter the name of the book that you want to search: "<<endl;
+    int flag = 0 ;        // Flag used to determine if book not found
+    char search_bname[100]; // To input which name needs to be sorted
+    cout << "Please enter the name of the book that you want to search: "<<endl; 
     cin >> search_bname;
 
     for (int i = 0; i < n; i++)
     {
-        if(strcmp(search_bname , s[i].book_name)==0)
+        if(strcmp(search_bname , s[i].book_name)==0) //Using strcmp to check if book is present
         {
-            cout<<" The book is present "<<endl;
-            cout<< s[i].book_name << "\t" <<s[i].no_copies<<endl;
-            flag++;
+            cout<<" The book is present "<<endl;     // If strcmp is 0, then book present
+            cout<< s[i].book_name << "\t" <<s[i].no_copies<<endl; //Showing book name and number of copies
+            flag++;   //Incrementing flag to signify that book found
         }
         
     }
-    if(flag == 0)
+    if(flag == 0) // No change in flag value.Hence, book absent
     {
         cout<< "The book could not be found. "<<endl;
     } 
 }
 
 
-void Search_auth () 
+void Search_auth () //Function to search by Author name
 {   
-    int flag;
-    char search_aname[100];
+    int flag; //To determine if name present or not
+    char search_aname[100];  // To input name to search for
     cout << "Please enter the name of the author that you want to search: "<<endl;
     cin >> search_aname;
 
     for (int i = 0; i < n; i++)
     {
-        if(strcmp(search_aname , s[i].auth_name)==0)
+        if(strcmp(search_aname , s[i].auth_name)==0) //Using strcmp to check. Present if value is 0.
         {
-            cout<< s[i].book_name << "\t" <<s[i].no_copies<<endl;
-            flag++;
+            cout<< s[i].book_name << "\t" <<s[i].no_copies<<endl; // Displaying all books from same author.
+            flag++;  //Incrementing flag to signify that author found.
         }
         
     } 
-    if(flag == 0)
+    if(flag == 0) // No change in flag value.Hence, author name not found.
     {
         cout<< "The name of this author is absent"<<endl;
     }
 }
 
 
-void Sort()
+void Sort() //Function to sort
 {
     int i,j;
-    char temp[100];
-    for(i=0;i<n;i++)
+    char temp[100];     //placeholder
+    for(i=0;i<n;i++)    //nested for loop to compare
     {
-      for(j=i+1;j<n;j++)
+      for(j=i+1;j<n;j++)     
       {
            if(strcmp(s[i].book_name, s[j].book_name)>0)
            {
@@ -113,7 +120,7 @@ void Sort()
            }
       } 
     }    
-    Display();
+    Display(); // Function called to print sorted list.
     cout<<"Successfully sorted ! "<<endl;
 }
 
@@ -121,7 +128,7 @@ void Sort()
 
 int main()
 {
-    int ch;
+    int ch; //To choose a case
 
     do
     {
@@ -130,11 +137,11 @@ int main()
         cout << "3.	Search the book in the library by its name." <<endl;
         cout << "4.	Search the books in the library by Author Name." <<endl;
         cout << "5.	Sort the list of books by its name." <<endl;
-        cout << "6. Exit." <<endl;
+        cout << "6.      Exit." <<endl;
         cout << "Please enter a choice: " <<endl;
         cin >> ch ;
         
-        switch(ch)
+        switch(ch)           //switvh case
         {
             case 1: Add_details() ;
                     break;
@@ -151,7 +158,7 @@ int main()
             default:std::cout<<"Invalid choice"<<std::endl;
         }
         
-    }while(ch != 6);
+    }while(ch != 6);  //To continue loop till Exit option is chosen.
 }
 
 
